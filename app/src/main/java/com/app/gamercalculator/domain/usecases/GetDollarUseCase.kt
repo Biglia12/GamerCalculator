ab6763559d10ea2e3a7ee01f49ca51c776fff540
Ezequiel Biglia <ezequielbiglia12@gmail.com>
2025-02-04 16:39:46 -0300

    mejora

--- app/src/main/java/com/app/gamercalculator/domain/usecases/GetDollarUseCase.kt
+++ app/src/main/java/com/app/gamercalculator/domain/usecases/GetDollarUseCase.kt
@@ -34,145 +34,82 @@ class GetDollarUseCase @Inject constructor(
         return DollarTaxes(
             name = dollarCard.name,
             date = formatDate(dollarCard.date),
-            dollarValue = formatMount(dollarValue),
-            taxIva = formatMount(taxIva),
-            taxArca = formatMount(taxArca),
-            mountTotal = formatMount(mountTotal),
+            dollarValue = "formatMount(dollarValue)",
+            taxIva = "formatMount(taxIva)",
+            taxArca = "formatMount(taxArca)",
+            mountTotal = "formatMount(mountTotal)",
             mountTotalTaxes = "0.00"
         )
     }
 
     suspend fun getDollarMep(inputNumber: String, moneyCurrency: String): DollarTaxes {
         val dollarMep = repository.getDollarMep()
-        val inputAmount = inputNumber.toDouble()
-
-        val countDollarMep = dollarMep.sell * inputAmount
-        val countDollarPesos = inputAmount / dollarMep.sell
-
-        return DollarTaxes(
-            name = dollarMep.name,
-            date = formatDate(dollarMep.date),
-            dollarValue = formatMount(when(moneyCurrency){
-                "ARS" -> inputAmount
-                "USD" -> countDollarMep
-                "EUR" -> 0.0
-                else -> {0.0}
-            }),
-            taxIva = "0.00",
-            taxArca = "0.00",
-            mountTotal =
-            formatMount(when(moneyCurrency){
-                "ARS" -> countDollarPesos
-                "USD" -> countDollarMep
-                "EUR" -> 0.0
-                else -> {0.0}
-            }),
-            //formatMount(if (isDollarChecked) countDollarMep else countDollarPesos),
-            mountTotalTaxes = "0.00"
-        )
+        return calculateDollarValues(dollarMep.name, dollarMep.sell, dollarMep.date, inputNumber, moneyCurrency)
     }
 
     suspend fun getDollarCripto(inputNumber: String, moneyCurrency: String): DollarTaxes {
         val dollarCripto = repository.getDollarCripto()
-        val inputAmount = inputNumber.toDouble()
-
-        val countDollarCripto = dollarCripto.sell * inputAmount
-        val countPesosCripto = inputAmount / dollarCripto.sell
-
-        return DollarTaxes(
-            name = dollarCripto.name,
-            date = formatDate(dollarCripto.date),
-            dollarValue =
-            formatMount(when(moneyCurrency){
-                "ARS" -> inputAmount
-                "USD" -> countDollarCripto
-                "EUR" -> 0.0
-                else -> {0.0}
-            }),
-            //formatMount(if (isDollarChecked) countDollarCripto else inputAmount),
-            taxIva = "0.00",
-            taxArca = "0.00",
-            mountTotal =
-            formatMount(when(moneyCurrency){
-                "ARS" -> countPesosCripto
-                "USD" -> countDollarCripto
-                "EUR" -> 0.0
-                else -> {0.0}
-            }),
-            //formatMount(if (isDollarChecked) countDollarCripto else countPesosCripto),
-            mountTotalTaxes = "0.00"
-        )
+        return calculateDollarValues(dollarCripto.name, dollarCripto.sell, dollarCripto.date, inputNumber, moneyCurrency)
     }
 
     suspend fun getDollarCardDigital(inputNumber: String, moneyCurrency: String): DollarTaxes {
         val dollarOfficial = repository.getDollarOfficial()
         val inputAmount = inputNumber.toDouble()
 
-        // Cálculo en dólares
+        // Impuestos en USD
         val taxIvaDollar = calculateTax(dollarOfficial.sell, inputAmount, 21)
         val taxArcaDollar = calculateTax(dollarOfficial.sell, inputAmount, 30)
-        val sumTaxesDollar = dollarOfficial.sell * inputAmount + taxIvaDollar + taxArcaDollar
-        val sumTaxesTotalDollar = taxIvaDollar + taxArcaDollar
+        val totalDollarWithTaxes = dollarOfficial.sell * inputAmount + taxIvaDollar + taxArcaDollar
+        val totalTaxesDollar = taxIvaDollar + taxArcaDollar
 
-        // Cálculo en pesos
+        // Impuestos en ARS
         val taxIvaPesos = calculateTax(inputAmount, 1.0, 21)
         val taxArcaPesos = calculateTax(inputAmount, 1.0, 30)
-        val sumTaxesPesos = inputAmount + taxIvaPesos + taxArcaPesos
-        val sumTaxesTotalPesos = taxIvaPesos + taxArcaPesos
+        val totalPesosWithTaxes = inputAmount + taxIvaPesos + taxArcaPesos
+        val totalTaxesPesos = taxIvaPesos + taxArcaPesos
 
         return DollarTaxes(
             name = dollarOfficial.name,
             date = formatDate(dollarOfficial.date),
-            dollarValue =
-            formatMount(when(moneyCurrency){
-                "ARS" -> inputAmount
-                "USD" -> dollarOfficial.sell * inputAmount
-                "EUR" -> 0.0
-                else -> {0.0}
-            }),
-            //formatMount(if (isDollarChecked) dollarOfficial.sell * inputAmount else inputAmount),
-            taxIva =
-            formatMount(when(moneyCurrency){
-                "ARS" -> taxIvaPesos
-                "USD" -> taxIvaDollar
-                "EUR" -> 0.0
-                else -> {0.0}
-            }),
-            //formatMount(if (isDollarChecked) taxIvaDollar else taxIvaPesos),
-            taxArca =
-            formatMount(when(moneyCurrency){
-                "ARS" -> taxArcaPesos
-                "USD" -> taxArcaDollar
-                "EUR" -> 0.0
-                else -> {0.0}
-            }),
-            //formatMount(if (isDollarChecked) taxArcaDollar else taxArcaPesos),
-            mountTotal =
-            formatMount(when(moneyCurrency){
-                "ARS" -> sumTaxesPesos
-                "USD" -> sumTaxesDollar
-                "EUR" -> 0.0
-                else -> {0.0}
-            }),
-            //formatMount(if (isDollarChecked) sumTaxesDollar else sumTaxesPesos),
-            mountTotalTaxes =
-            formatMount(when(moneyCurrency){
-                "ARS" -> sumTaxesPesos
-                "USD" -> sumTaxesTotalDollar
-                "EUR" -> 0.0
-                else -> {0.0}
-            })
-            //formatMount(if (isDollarChecked) sumTaxesTotalDollar else sumTaxesTotalPesos)
+            dollarValue = formatAmount(getValueByCurrency(moneyCurrency, inputAmount, dollarOfficial.sell)),
+            taxIva = formatAmount(getValueByCurrency(moneyCurrency, taxIvaPesos, taxIvaDollar)),
+            taxArca = formatAmount(getValueByCurrency(moneyCurrency, taxArcaPesos, taxArcaDollar)),
+            mountTotal = formatAmount(getValueByCurrency(moneyCurrency, totalPesosWithTaxes, totalDollarWithTaxes)),
+            mountTotalTaxes = formatAmount(getValueByCurrency(moneyCurrency, totalTaxesPesos, totalTaxesDollar))
         )
     }
 
+    private fun calculateDollarValues(name: String, sellPrice: Double, date: String, inputNumber: String, moneyCurrency: String): DollarTaxes {
+        val inputAmount = inputNumber.toDouble()
+
+        val amountInUsd = sellPrice * inputAmount
+        val amountInArs = inputAmount / sellPrice
+
+        return DollarTaxes(
+            name = name,
+            date = formatDate(date),
+            dollarValue = formatAmount(getValueByCurrency(moneyCurrency, inputAmount, amountInUsd)),
+            taxIva = "0.00",
+            taxArca = "0.00",
+            mountTotal = formatAmount(getValueByCurrency(moneyCurrency, amountInArs, amountInUsd)),
+            mountTotalTaxes = "0.00"
+        )
+    }
+
+    private fun getValueByCurrency(currency: String, arsValue: Double, usdValue: Double): Double {
+        return when (currency) {
+            "ARS" -> arsValue
+            "USD" -> usdValue
+            "EUR" -> 0.0
+            else -> 0.0
+        }
+    }
 
     private fun calculateTax(base: Double, amount: Double, taxPercentage: Int): Double {
         return (base * amount * taxPercentage) / 100
     }
 
-
-    private fun formatMount(value: Double): String {
+    private fun formatAmount(value: Double): String {
         val formatter = NumberFormat.getNumberInstance(Locale("es")).apply {
             maximumFractionDigits = 2
             minimumFractionDigits = 2
